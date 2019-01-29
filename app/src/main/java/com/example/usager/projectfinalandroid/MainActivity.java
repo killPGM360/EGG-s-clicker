e9a4b30a07503d9c4db9779fbf4980816c48e607
Philippe Larose <plarose900@gmail.com>
2019-01-29 17:14:01 -0500

    egg fonctionnel

--- app/src/main/java/com/example/usager/projectfinalandroid/MainActivity.java
+++ app/src/main/java/com/example/usager/projectfinalandroid/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.usager.projectfinalandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int m_compteur=0;
+    int m_multipleManuel=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onEgg(View view)
+    {
+
+        incrementCompteur(m_multipleManuel);
+    }
+    public void incrementCompteur(int multiple)
+    {
+        m_compteur=m_compteur+multiple;
+        TextView compteur=(TextView) findViewById(R.id.txtCompteur);
+        compteur.setText(String.valueOf(m_compteur));
+    }
 }
+
+
