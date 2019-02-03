862c869b0f658bfdaaa068ffd9cdd8be76812a27
Philippe <plarose900@gmail.com>
2019-02-03 17:37:15 -0500

    accueil

---
+++ app/src/main/java/com/example/usager/projectfinalandroid/ActivityAccueil.java
@@ -0,0 +1,13 @@
+package com.example.usager.projectfinalandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityAccueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+    }
+}
