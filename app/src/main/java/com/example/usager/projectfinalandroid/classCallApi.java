bdf50c3055a88fbdb002aae4dec3a1006cc4ee17
phil <plarose900@gmail.com>
2019-02-12 17:10:08 -0500

    debut add class pour post

---
+++ app/src/main/java/com/example/usager/projectfinalandroid/classCallApi.java
@@ -0,0 +1,43 @@
+package com.example.usager.projectfinalandroid;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class classCallApi {
+
+    public void getUser(String username, String mdp, Context context)
+    {
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(context);
+        String url = "";
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("Field", "Value"); //Add the data you'd like to send to the server.
+                return MyData;
+            }
+        };
+
+
+            MyRequestQueue.add(MyStringRequest);
+    }
+
+}
